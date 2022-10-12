e08c3b645ac95118b6940786e0a23e7569bad111
rsn93323 <rupertmarty.navarro.954@my.csun.edu>
2022-10-12 00:50:15 -0700

    init

---
+++ src/app/fetch-data/fetch-data.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-fetch-data',
+  templateUrl: './fetch-data.component.html'
+})
+export class FetchDataComponent {
+  public forecasts: WeatherForecast[] = [];
+
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>(environment.baseUrl + 'WeatherForecast/GetWeatherForecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
